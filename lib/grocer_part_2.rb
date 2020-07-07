5b51e77d6a766d586268af99061c71ae98e95fb1
Kiri Um <kirirotha@gmail.com>
2020-07-07 22:28:15 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -13,7 +13,7 @@ def apply_coupons(cart, coupons)
         #puts cart_item
         discounted_price = coupon_item[:cost] / coupon_item[:num]
         discounted_item = {:item => cart_item[:item] + ' W/COUPON', :price => discounted_price, :clearence => true, :count => coupon_item[:num]}
-        #if cart_item[:count] > 0
+        #if cart_item[:count] = 0
           #new_cart << cart_item
         #end  
         new_cart << discounted_item
