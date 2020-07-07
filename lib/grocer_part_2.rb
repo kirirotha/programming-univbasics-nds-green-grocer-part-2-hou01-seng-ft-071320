1dc751dfbd95f3a0d1d9aebb3777bd906d7e41cf
Kiri Um <kirirotha@gmail.com>
2020-07-07 22:56:52 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -45,7 +45,7 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   puts consolidate_cart(cart)
-  #puts apply_coupons(cart, coupons)
+  puts apply_coupons(cart, coupons)
   #puts apply_clearance(cart)
 end
 
